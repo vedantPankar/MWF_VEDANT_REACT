1cc05600b20ab0325a99cbcd862c24323c3864ec
Vedant Pankar <vedantpankar2@gmail.com>
2025-11-27 11:48:16 +0530

    understand useEffect

--- 02hooks/src/UseEffect.jsx
+++ 02hooks/src/UseEffect.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function UseEffect() {
+  useEffect(() => {
+    console.log("this runs when the component mounts");
+  }, []);
   return <div>UseEffect</div>;
 }
 

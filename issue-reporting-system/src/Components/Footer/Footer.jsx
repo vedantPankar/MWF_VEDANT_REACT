51b8585fc4fcd3b8061bad6c8a29600a4c4f2775
Vedant Pankar <vedantpankar2@gmail.com>
2025-11-28 12:58:17 +0530

    Reset

--- issue-reporting-system/src/Components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Footer() {
-  return <div>Made with ♥️ by Vedant Pankar</div>;
-}
-
-export default Footer;
--- issue-reporting-system/src/Components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import ReportIssue from "./ReportIssue";
-import Dashboard from "./Dashboard";
-
-export { Header, Footer, ReportIssue, Dashboard };
